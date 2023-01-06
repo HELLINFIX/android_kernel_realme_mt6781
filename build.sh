0d900d41c79716dfbf573badeb7f41ae42131b05
HELLINFIX <mridulsharma57570@gmail.com>
2023-01-06 17:39:28 +0000

    build_sh: switch to azure clang 15

--- build.sh
+++ build.sh
@@ -9,21 +9,24 @@ ccache -M 100G
 export ARCH=arm64
 export KBUILD_BUILD_HOST=COSMOS
 export KBUILD_BUILD_USER="INFIX"
-git clone --depth=1 https://github.com/Project-Elixir/android_prebuilts_clang_host_linux-x86_clang-r437112 clang
-git clone --depth=1 https://github.com/DrtSinX98/prebuilts_gcc_linux-x86_aarch64_aarch64-linaro-7 los-4.9-64
-git clone --depth=1 https://github.com/DrtSinX98/linaro_arm-linux-gnueabihf-7.5 los-4.9-32
+git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang.git clang
 
 [ -d "out" ] && rm -rf out || mkdir -p  && make mrproper
 
 make O=out ARCH=arm64 spaced_defconfig
 
-PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}" \
+PATH="${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}" \
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
                       CC="clang" \
+                      LD=ld.lld \
+		      AR=llvm-ar \
+		      NM=llvm-nm \
+		      OBJCOPY=llvm-objcopy \
+		      OBJDUMP=llvm-objdump \
                       CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-gnu-" \
-                      CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-gnueabihf-" \
+                      CROSS_COMPILE="${PWD}/clang/bin/aarch64-linux-gnu-" \
+                      CROSS_COMPILE_ARM32="${PWD}/clang/bin/arm-linux-gnueabi-" \
                       CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee error.log 
 }
 
@@ -32,9 +35,9 @@ function zupload()
 git clone --depth=1 https://github.com/HELLINFIX/AnyKernel3.git AnyKernel
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
-zip -r9 Cosmos-kernel-1.0-spaced.zip *
+zip -r9 Infix-kernel-1.0-spaced.zip *
 curl -sL https://git.io/file-transfer | sh
-./transfer wet Cosmos-kernel-1.0-spaced.zip
+./transfer wet Infix-kernel-1.0-spaced.zip
 }
 
 compile
